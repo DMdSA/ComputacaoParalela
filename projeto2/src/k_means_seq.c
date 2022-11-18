9256d2d072322fb1842e3c6e8988370946970bd1
Diogo Miguel Silva Araújo <diogomiguel534@gmail.com>
2022-11-18 17:44:40 +0000

    makefile & seq code updated

--- projeto2/src/k_means_seq.c
+++ projeto2/src/k_means_seq.c
@@ -48,100 +48,124 @@ int initialize(int n_points, int n_clusters, int n_threads) {
  */
 void populate(int n_points, int n_clusters) {
 
-    // default rand seed
+   // default rand seed
     srand(10);
     
     // random points
-    for (int i = 0; i < n_points; i++) {
-        
+    int i = 0;
+    for (i = 0; i < n_clusters; i++) {
+
         float x = (float) rand() / RAND_MAX, y = (float) rand() / RAND_MAX;
         RANDOM_SAMPLE[i] = (struct point) {.x = x, .y = y, .k = -1};
-    }
-
-
-    // clusters
-    for (int i = 0; i < n_clusters; i++) {
-
-        float x = (RANDOM_SAMPLE[i]).x, y = (RANDOM_SAMPLE[i]).y;
         CLUSTERS[i] = (struct cluster) {.x = x, .y = y, .dimension = 0};
+    }
+    
+    for (int j = i; j < n_points; j++) {
+
+        float x = (float) rand() / RAND_MAX, y = (float) rand() / RAND_MAX;
+        RANDOM_SAMPLE[j] = (struct point) {.x = x, .y = y, .k = -1};
     }
 }
 
-
+/**
+ * @brief calculates the euclidian distance between a given point and cluster
+ * 
+ * @param point given point's struct
+ * @param cluster given cluster's struct
+ * @return float calculated euclidian distance
+ */
 float euclidian_distance(struct point point, struct cluster cluster) {
 
     float x = point.x-cluster.x, y = point.y-cluster.y;
     return (sqrtf(x*x + y*y));
 }
 
-
-void updateSamples(int samples, int klusters, float* centroid_mean_array) {
+/**
+ * @brief goes through all random samples and calculates their new associated cluster
+ * 
+ * @param samples random samples array
+ * @param klusters clusters array
+ * @param centroid_mean_array auxiliar array for further calculation of clusters' new centroids
+ */
+int updateSamples(int samples, int klusters, float* centroid_mean_array) {
 
     float min_dist = 0.0f;
-    int min_index = 0;
-    int j = 0;
+    int min_index = 0, j = 0, changedFlag = 0;
 
-            for (int i = 0; i < samples; i++) {
+    for (int i = 0; i < samples; i++) {
 
-                struct point point = RANDOM_SAMPLE[i];
-                min_dist = euclidian_distance(point, CLUSTERS[0]);
-                min_index = 0;
+        struct point point = RANDOM_SAMPLE[i];
+        min_dist = euclidian_distance(point, CLUSTERS[0]);
+        min_index = 0;
                 
-                for (int j = 1; j < klusters; j++) {
-                    
-                    int id = omp_get_thread_num();
-                    struct cluster cluster = CLUSTERS[j];
-
-                    float dist = euclidian_distance(point, cluster);
-
-                    if (dist < min_dist) {
-
-                        min_dist = dist;
-                        min_index = j;
-                    }
-                }
-
-                //point.k = min_index;
-                CLUSTERS[min_index].dimension++;
-
-                centroid_mean_array[min_index*2] += point.x;
-                centroid_mean_array[min_index*2+1] += point.y; 
+        for (j = 1; j < klusters; j++) {
+            
+            float dist = euclidian_distance(point, CLUSTERS[j]);
+            if (dist < min_dist) {
+                min_dist = dist;
+                min_index = j;
             }
+        }                
+                
+        // update point's cluster (only needed if we want to know to which cluster it belongs ...)
+        RANDOM_SAMPLE[i].k = min_index;
+        
+        // if a point's cluster is changed, we need to aknowledge it
+        changedFlag += (point.k != min_index) ? 1 : 0;
+        
+        CLUSTERS[min_index].dimension++;
+        
+        centroid_mean_array[min_index*2] += point.x;
+
+        centroid_mean_array[min_index*2+1] += point.y; 
+                
     }
+
+    // we need to know if any point was changed from it's previous cluster
+    return changedFlag;
 }
 
+/**
+ * @brief updates a cluster's centroid
+ * 
+ * @param cluster given cluster
+ * @param cluster_index cluster's id
+ * @param centroid_mean_array auxiliar array with point's values sum
+ */
+void update_cluster(struct cluster* cluster, int cluster_index, float* centroid_mean_array) {
 
-int update_cluster(struct cluster* cluster, int cluster_index, float* centroid_mean_array) {
+    // as divisions are expensible,
+    float dimension = (1.0f / (float)(*cluster).dimension);
 
-    int index = cluster_index*2;
-
-    float x = (*cluster).x, y = (*cluster).y;
-    (*cluster).x = centroid_mean_array[index] / (float)(*cluster).dimension;
-    (*cluster).y = centroid_mean_array[index+1] / (float)(*cluster).dimension;
+    // sum instead of multiply by two
+    int index = cluster_index + cluster_index;
+    
+    // centroid_mean_array = [C0.X, C0.Y,  C1.X, C2.Y,  C3.X, C3.Y,  ...]
+    // Cluster#0 = {0,1}, Cluster#1 = {2, 3}, Cluster#2 = {4, 5}
+    // new centroid calculated by dividing sum of points by cluster's dimension
+    (*cluster).x = centroid_mean_array[index] * dimension;
+    (*cluster).y = centroid_mean_array[index+1] * dimension;
 
     // clear the auxiliar values
     centroid_mean_array[index] = 0.0f;
     centroid_mean_array[index+1] = 0.0f;
 
-    // clear cluster dimension for next iteration
-    //(*cluster).dimension = 0;
-
-    if (cluster->x == x && cluster->y == y) return 0;
-    return 1;
 }
 
-int update_clusters(int samples, int klusters, int n_threads, float* centroid_mean_array) {
 
-    int flag = 0;
-    
+/**
+ * @brief main function for calculation of all clusters' centroids
+ * 
+ * @param klusters number of clusters
+ * @param centroid_mean_array auxiliar array with point's values sum
+ */
+void updateClusters(int klusters, float* centroid_mean_array) {
+
     for (int i = 0; i < klusters; i++) {
         
-        flag += update_cluster(&CLUSTERS[i], i, centroid_mean_array);   
+        update_cluster(&CLUSTERS[i], i, centroid_mean_array);   
     }
-
-    return flag;
 }
-
 
 
 /**
@@ -152,11 +176,9 @@ int update_clusters(int samples, int klusters, int n_threads, float* centroid_me
  */
 void main_arg_control(int argc, char* argv[]) {
 
-    if (argc != 4) {
+    if (argc != 3) {
 
-        printf("\n#> [k_means:error]\n\t%s <number of points> <number of clusters> <number of threads>\n\n"
-        , argv[0]);
-        
+        printf("\n#> [k_means:error]\n\t%s <number of points> <number of clusters>\n\n", argv[0]);
         exit(1);
     }
 }
@@ -171,7 +193,6 @@ int main(int argc, char* argv[]) {
     int n_points = 0, n_clusters = 0, n_threads = 0;
     sscanf(argv[1], "%d", &n_points);
     sscanf(argv[2], "%d", &n_clusters);
-    sscanf(argv[3], "%d", &n_threads);
 
     if (n_clusters > n_points) {
 
@@ -187,13 +208,12 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < centr_means_size; i++) {
         CENTR_MEANS[i]=0.0f;
     }
-        
 
     int end_flag = 1, n_loops = 0;
 
+    float begin = omp_get_wtime();
     initialize(n_points, n_clusters, n_threads);
     populate(n_points, n_clusters);
-
 
     for (end_flag = 1; (end_flag && n_loops <= 20); n_loops++) {
 
@@ -202,11 +222,14 @@ int main(int argc, char* argv[]) {
             CLUSTERS[i].dimension = 0;
         }
         
-        updateSamples(n_points, n_clusters, CENTR_MEANS);
-        end_flag = update_clusters(n_points, n_clusters, n_threads, CENTR_MEANS);
+        end_flag = updateSamples(n_points, n_clusters, CENTR_MEANS);
+        updateClusters(n_clusters, CENTR_MEANS);
     }
     --n_loops;
     
+
+    float end = omp_get_wtime();
+    float time_spent = (end-begin);
     printf("\n#> exec: %lf\n\n", time_spent);
     
     printf("N = %d, K = %d, T = %d", n_points, n_clusters, n_threads);
